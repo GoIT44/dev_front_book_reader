5723e44f0f30813231db527f0af43bb34914ecd3
Oleg Serdiuk <ol.serduk@gmail.com>
2022-02-17 16:38:38 +0200

    html list done

--- src/components/TrainingSection/BookList/Booklist.js
+++ src/components/TrainingSection/BookList/Booklist.js
@@ -1,75 +1,72 @@
 // import { style } from "@mui/system";
 // import React, { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-// import useMedia from "../../hooks/useMedia/useMedia.js";
-import BooksListItem from "./BookListItem/BooksListItem";
+import iconSprite from "../../../images/icons-sprite.svg";
+import BooksListItem from "./BooksListItem/BooksListItem";
 import styles from "./Booklist.module.css";
+import style from "./BooksListItem/BooksListItem.module.css";
 
 const Booklist = () => {
-  // const [screenWidth, setScreenWidth] = useState(null);
-  // const [books, setBooks] = useState([]);
-  // const screenSize = useMedia();
-
   const books = [
     {
       title: "Book",
       year: 2002,
       author: "Author",
       pages: 356,
-      id: 1,
     },
     {
       title: "Animate Book",
       year: 2007,
       author: "Animate Author",
       pages: 549,
-      id: 2,
     },
     {
       title: "Interesting Book",
       year: 2000,
       author: "Ivan Franko",
       pages: 326,
-      id: 3,
     },
   ];
 
-  // useEffect(() => {
-  //   setScreenWidth(window.screen.width);
-  // }, [screenWidth]);
-
   return (
-    <>
-      <ul className={styles.title__list}>
-        <li key={nanoid()} className={styles.title__listItem}>
-          Назва книги
+    <ul className={styles.bookList}>
+      <li className={styles.bookItemTitle}>
+        <ul className={styles.bookDescrList}>
+          <li className={styles.bookDescrItem}>
+            <p className={styles.subTitle}>Назва книги</p>
+          </li>
+          <li className={styles.bookDescrItem}>
+            <p className={styles.subTitle}>Автор</p>
+          </li>
+          <li className={styles.bookDescrItem}>
+            <p className={styles.subTitle}>Рік</p>
+          </li>
+          <li className={styles.bookDescrItem}>
+            <p className={styles.subTitle}>Стор</p>
+          </li>
+        </ul>
+      </li>
+      {books.length > 0 ? (
+        books.map((book) => (
+          <BooksListItem
+            key={nanoid()}
+            title={book.title}
+            author={book.author}
+            year={book.year}
+            pages={book.pages}
+          />
+        ))
+      ) : (
+        <li className={style.bookItem}>
+          <div className={style.iconWrapper}>
+            <svg className={style.iconBookList} width="22px" height="17px">
+              <use xlinkHref={`${iconSprite}#icon-book`} />
+            </svg>
+            <span className={style.titleBook}> ...</span>
+          </div>
         </li>
-        <li key={nanoid()} className={styles.title__listItem}>
-          Автор
-        </li>
-        <li key={nanoid()} className={styles.title__listItem}>
-          Рік
-        </li>
-        <li key={nanoid()} className={styles.title__listItem}>
-          Стор
-        </li>
-      </ul>
-      <ul className={styles.bookList}>
-        {books.length > 0 ? (
-          books.map((book) => (
-            <BooksListItem
-              key={nanoid()}
-              title={book.title}
-              author={book.author}
-              year={book.year}
-              pages={book.pages}
-            />
-          ))
-        ) : (
-          <BooksListItem />
-        )}
-      </ul>
-    </>
+      )}
+    </ul>
   );
 };
 
