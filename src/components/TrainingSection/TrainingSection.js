64151fc00601901a7c57a4fa4f59d54145025b61
Oleg Serdiuk <ol.serduk@gmail.com>
2022-02-19 11:51:02 +0200

    training page start to create

--- src/components/TrainingSection/TrainingSection.js
+++ src/components/TrainingSection/TrainingSection.js
@@ -2,12 +2,10 @@
 // import { useState } from "react";
 // import { useSelector, useDispatch } from "react-redux";
 
-// import Booklist from "./BookList/Booklist";
+import SideBarMyGoal from "./SideBarMyGoal/SideBarMyGoal";
 
-// import ResultBox from "../../example/ResultBox";
 // import TrainingModalBtn from "../TrainingModalBtn/TrainingModalBtn";
 
-import Box from "@mui/material/Box";
 import { ReactComponent as ReactLogo } from "../../images/training_page_icons/arrow-back.svg";
 import Form from "./Form/Form";
 import styles from "./TrainingSection.module.css";
@@ -23,6 +21,7 @@ const TrainingSection = () => {
             <ReactLogo />
           </div>
         </button>
+        <SideBarMyGoal />
         <div className={styles.section__centeredWrap}>
           <div className={styles.section__titleWrap}>
             <h2 className={styles.section__title}>Моє тренування</h2>
@@ -32,9 +31,9 @@ const TrainingSection = () => {
           </div>
         </div>
       </section>
+
       {/* <Booklist /> */}
 
-      {/* <ResultBox /> */}
       {/* <TrainingModalBtn /> */}
     </>
   );
