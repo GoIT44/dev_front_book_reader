f27361cb122ca80e1e38d22795ea68a6d86ce477
Vitalii Shaiuk <68068673+vitalis-virtus@users.noreply.github.com>
2022-02-20 13:12:43 +0200

    решаю конфликты

--- src/pages/LibraryPage/LibraryPage.js
+++ src/pages/LibraryPage/LibraryPage.js
@@ -3,12 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import LibraryModal from '../../components/LibraryModal/LibraryModal';
 import LibraryForm from '../../components/LibraryForm'
 import {fetchToken} from '../../redux/auth/auth-selectors'
-import {
-    getWillRead,
-    getNowRead,
-    getFinishRead,
-    // getUserTraining,
-} from '../../redux/selectors/bookSelector';
 import { getUsersBooksOperation } from '../../redux/operations/bookOperation';
 
 
@@ -17,29 +11,13 @@ const LibraryPage =() => {
     const isAuth = useSelector(fetchToken);
     console.log("Is auth" , isAuth);
 
-    // const booksWillRead = useSelector(getWillRead);
-    // const bookNowRead = useSelector(getNowRead);
-    // const bookFinished = useSelector(getFinishRead);
-    // const isTraining = useSelector(getUserTraining);
-
     useEffect(() => {
         if(isAuth) {dispatch(getUsersBooksOperation())};    
         // eslint-disable-next-line
     }, []);
 
     return(<div>
-        <h1>Library Page </h1>                       {/* <LibraryForm />
-                {bookFinished.length > 0 && <ReadBooks />}
-                {bookNowRead.length > 0 && <ReadingBooks />}
-                {booksWillRead.length > 0 || bookNowRead.length > 0 || bookFinished.length > 0 ? (
-                    !!booksWillRead.length && <WillReadBooks />
-                ) : (
-                    <LibraryModal />
-                )}
-                {booksWillRead.length > 0 && isTraining === null && (
-                    <StartTrainingBtn />
-                )} */}
-    {/* <LibraryForm/> */}
+        <h1>Library Page </h1>  
     </div>)
 }
 
