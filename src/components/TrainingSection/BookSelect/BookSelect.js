15bbb2ef5ce4da58ed6d58385bb48a24439b49bb
Oleg Serdiuk <ol.serduk@gmail.com>
2022-02-23 21:04:12 +0200

    modal + styles done

--- src/components/TrainingSection/BookSelect/BookSelect.js
+++ src/components/TrainingSection/BookSelect/BookSelect.js
@@ -1,42 +1,54 @@
-import styles from './BookSelect.module.css';
-import {useState, useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addBookTrainingOperation } from '../../../redux/trainingPlan/trainingPlanOperations';
-import { getBookList } from '../../../redux/trainingPlan/trainingPlanSelectors';
+import styles from "./BookSelect.module.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addBookTrainingOperation } from "../../../redux/trainingPlan/trainingPlanOperations";
+import { getBookList } from "../../../redux/trainingPlan/trainingPlanSelectors";
 
-const BookSelect = ({bookSelect, getbook}) => {
-    
-    const dispatch = useDispatch()
-    const bookPrev = useSelector(getBookList)
-    const [currentBook, setCurrentBook] = useState("");
-    const [arrBook, setArrBook] = useState([]);
-    const onChange = (e) => {
-        setCurrentBook(e.target.value)
-    }
-    const onSubmit = (e) => {
-        e.preventDefault()
-        !bookPrev.includes(currentBook) && dispatch(addBookTrainingOperation(currentBook))
-        setCurrentBook("")
-            
-    }
-    return (
-        <div className={styles.bookSelect}>
-            <div className={styles.select__wrap}>
-                <form className={styles.form} onSubmit={onSubmit}>
-                    <select className={styles.selectBook} value={currentBook} onChange={onChange}>
-                    <option value="..." selected>....</option>
-                        {bookSelect && bookSelect.map((book)=>(
-                            <option id={book._id} value={book._id}>{book.bookTitle}</option>
-                        ))}
-                        <option></option>
-                    </select>
-                    <div className={styles.btn__wrap}>
-                        <button type="submit" className={styles.btn__add}>Додати</button>
-                    </div>
-                </form>
-            </div>
+const BookSelect = ({ bookSelect }) => {
+  const dispatch = useDispatch();
+  const bookPrev = useSelector(getBookList);
+  const [currentBook, setCurrentBook] = useState("");
+  const [arrBook, setArrBook] = useState([]);
+  const onChange = (e) => {
+    setCurrentBook(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    !bookPrev.includes(currentBook) &&
+      dispatch(addBookTrainingOperation(currentBook));
+    setCurrentBook("");
+  };
+  return (
+    <div className={styles.bookSelect}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={styles.select__wrap}>
+          <label className={styles.labelSelect} forhtml="selectBook">
+            Обрати книги з бiблiотеки
+          </label>
+          <select
+            type="select"
+            className={styles.select}
+            value={currentBook}
+            onChange={onChange}
+            id="selectBook"
+          >
+            <option value="...">....</option>
+            {bookSelect &&
+              bookSelect.map((book) => (
+                <option key={book._id} id={book._id} value={book._id}>
+                  {book.bookTitle}
+                </option>
+              ))}
+          </select>
         </div>
-    );
-}
+        <div className={styles.btn__wrap}>
+          <button type="submit" className={styles.btn__add}>
+            Додати
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default BookSelect;
--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -1,96 +1,97 @@
 // import React from 'react'
-import TrainingSection from "../../components/TrainingSection/TrainingSection";
-import Navbar from "../../components/Navbar/Navbar.jsx";
 import styles from "./TrainingPage.module.css";
-import {SideBarStatistics} from '../../components/SideBarStatistics/SideBarStatistics'
 import SideBarMyGoal from "../../components/TrainingSection/SideBarMyGoal/SideBarMyGoal";
 import Booklist from "../../components/TrainingSection/BookList/Booklist";
 import Chart from "../../components/TrainingSection/Chart/Chart";
 import TrainingPeriodBlock from "../../components/TrainingSection/TrainingPeriodBlock/TrainingPeriodBlock";
 import BookSelect from "../../components/TrainingSection/BookSelect/BookSelect";
 import TitleSection from "../../components/TrainingSection/Form/TitleSection/TitleSection";
-import axios from "axios";
-import {getLibraryInfo} from '../../services/bookApi';
+import { getLibraryInfo } from "../../services/bookApi";
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addBookTrainingOperation } from "../../redux/trainingPlan/trainingPlanOperations";
-import { getBookList, getDateTraining } from "../../redux/trainingPlan/trainingPlanSelectors";
+import { useSelector } from "react-redux";
+import {
+  getBookList,
+  getDateTraining,
+} from "../../redux/trainingPlan/trainingPlanSelectors";
 import { addTraining } from "../../services/training";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 import { useHistory } from "react-router-dom";
-import ModalOpenBtn from '../../components/TrainingSection/ModalOpenBtn/ModalOpenBtn';
-import FormMob from '../../components/TrainingSection/FormMob/FormMob';
+import ModalOpenBtn from "../../components/TrainingSection/ModalOpenBtn/ModalOpenBtn";
+import FormMob from "../../components/TrainingSection/FormMob/FormMob";
 
 const TrainingPage = () => {
   useEffect(async () => {
-    const book = await getLibraryInfo()
-    console.log(book)
-    setBookSelect(book)
-  }, [])
-  const [isHidden, setIsHidden] = useState(true)
+    const book = await getLibraryInfo();
+    setBookSelect(book);
+  }, []);
+  const [isHidden, setIsHidden] = useState(true);
   const history = useHistory();
-  const bookTrain = useSelector(getBookList)
-  const dateTrain = useSelector(getDateTraining)
-  const dispatch = useDispatch()
-  const [bookSelect, setBookSelect] = useState([]) 
+  const bookTrain = useSelector(getBookList);
+  const dateTrain = useSelector(getDateTraining);
+  const [bookSelect, setBookSelect] = useState([]);
   const training = {
     startTrain: dateTrain.startTrain,
     endTrain: dateTrain.endTrain,
     booksTrain: bookTrain,
-  }
-  const amountDay = Math.floor((new Date(dateTrain.endTrain)-new Date(dateTrain.startTrain)) / 1000/60/60/24);
-  console.log(amountDay)
-  const sendTrain = async (e)=>{
+  };
+  const amountDay = Math.floor(
+    (new Date(dateTrain.endTrain) - new Date(dateTrain.startTrain)) /
+      1000 /
+      60 /
+      60 /
+      24
+  );
+  const sendTrain = async (e) => {
     try {
-      const res = await addTraining(training)
-      history.push('/statistic')
+      const res = await addTraining(training);
+      history.push("/statistic");
     } catch (error) {
-      Notify.failure(error.message.message)
+      Notify.failure(error.message.message);
     }
-    // dispatch(addBookTrainingOperation())
-    
-  }
+  };
   const onClick = (e) => {
-    setIsHidden((prev)=>!prev)
-  }
-  
-  
-  const arrSelectBooks = bookSelect.filter((book)=>bookTrain.includes(book._id));
+    setIsHidden((prev) => !prev);
+  };
 
-  
+  const arrSelectBooks = bookSelect.filter((book) =>
+    bookTrain.includes(book._id)
+  );
 
-  
   return (
     <>
       <div className={styles.container}>
         <section className={styles.sectionAddTraining}>
           <div className={styles.grids}>
             <div className={styles.gridsSideBar}>
-              <SideBarMyGoal day={amountDay} book={bookTrain.length}/>
+              <SideBarMyGoal day={amountDay} book={bookTrain.length} />
             </div>
-            
+
             <div className={styles.gridsform}>
               <div className={styles.wrapperForm}>
-                <TitleSection/>
-                <TrainingPeriodBlock/>
-                <BookSelect bookSelect={bookSelect}/>
+                <TitleSection />
+                <TrainingPeriodBlock />
+                <BookSelect bookSelect={bookSelect} />
               </div>
-              <Booklist bookList={arrSelectBooks}/>
-                {
-                  typeof(training.startTrain) === "string" &&
-                      typeof(training.endTrain) === "string" &&
-                      !!training.booksTrain.length &&
+              <Booklist bookList={arrSelectBooks} />
+              {typeof training.startTrain === "string" &&
+                typeof training.endTrain === "string" &&
+                !!training.booksTrain.length && (
                   <div className={styles.btn__wrap}>
-                      <button className={styles.btn} onClick={sendTrain}>Почати тренування</button>
+                    <button className={styles.btn} onClick={sendTrain}>
+                      Почати тренування
+                    </button>
                   </div>
-                }
-              <Chart/>
-              <ModalOpenBtn onClick={onClick}/>
-              <FormMob onClick={onClick} bookSelect={bookSelect} isHidden={isHidden}/>
+                )}
+              <Chart />
+              <ModalOpenBtn onClick={onClick} />
+              <FormMob
+                onClick={onClick}
+                bookSelect={bookSelect}
+                isHidden={isHidden}
+              />
             </div>
           </div>
         </section>
-        {/* <TrainingSection /> */}
       </div>
     </>
   );
